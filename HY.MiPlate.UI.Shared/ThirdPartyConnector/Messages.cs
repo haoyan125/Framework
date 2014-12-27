2c9607fc38e2a7226344a2ed057683d5489ded98
haoyan125 <haoyan123@hotmail.com>
2014-12-28 02:24:52 +0800

    add message model

---
+++ HY.MiPlate.UI.Shared/ThirdPartyConnector/Messages.cs
@@ -0,0 +1,19 @@
+﻿namespace HY.MiPlate.UI.Shared.ThirdPartyConnector
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Messages
+  {
+    public string Access_Token { get; set; }
+
+    public string Errcode { get; set; }
+
+    public string Errmsg { get; set; }
+
+    public int Expires_in { get; set; }
+  }
+}
