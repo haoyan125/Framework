9c090f6fc7bfeac56da0c8ee294daa82f4cdcc24
haoyan125 <haoyan123@hotmail.com>
2014-12-28 01:40:46 +0800

    add weixin config model and test trigger of team city

--- HY.MiPlate.UI.Shared/ThirdPartyConnector/WeixinToken.cs
+++ HY.MiPlate.UI.Shared/ThirdPartyConnector/WeixinToken.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HY.MiPlate.UI.Shared.ThirdPartyConnector
+﻿namespace HY.MiPlate.UI.Shared.ThirdPartyConnector
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
   public class WeixinToken
   {
     public string TokenKey { get; set; }
 
     public long OrganiztionId { get; set; }
+  }
+
+  public class DeveloperOId
+  {
+    public string AppId { get; set; }
+
+    public string AppSecret { get; set; }
+
+    public long OrganiztionId { get; set; }
+  }
+
+  public class ServerConfig
+  {
+    public string URL { get; set; }
+
+    public string Token { get; set; }
+
+    public string EncodingAESKey { get; set; }
+
+    public int EncriptType { get; set; }
+
+    public long OrganiztionId { get; set; }
+  }
+
+  public enum EncriptType
+  {
+    PlainTextMode = 1,
+    CompatibilityMode = 2,
+    SafeMode = 3
   }
 }
